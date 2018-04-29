f8ab2cc1df38b08a95e88889f1953ea0b561dfb3
CryFar <Hamsik123159@gmail.com>
2018-04-29 21:38:06 +0200

    Trying some javascripts

---
+++ Scripts/contact.js
@@ -0,0 +1,41 @@
+function contact() {
+    document.getElementById("heading").innerHTML = "Contact";
+	
+	document.getElementById("underheading").innerHTML = "Something under heading"
+
+    document.getElementById("headingtwo").innerHTML = "Contact on something";
+
+    document.getElementById("paragraf1").innerHTML = "E-mail: something@something.something";
+
+    document.getElementById("paragraf2").innerHTML = "Phone: 1234 123 123";
+
+    document.getElementById("paragraf3").innerHTML = "";
+}
+
+function home() {
+    document.getElementById("heading").innerHTML = "Something";
+	
+	document.getElementById("underheading").innerHTML = "Something under heading"
+
+    document.getElementById("headingtwo").innerHTML = "We do something";
+
+    document.getElementById("paragraf1").innerHTML = "We do really something";
+
+    document.getElementById("paragraf2").innerHTML = "Like this";
+
+    document.getElementById("paragraf3").innerHTML = "And that";
+}
+
+function about() {
+    document.getElementById("heading").innerHTML = "Contact";
+	
+	document.getElementById("underheading").innerHTML = "Something under heading"
+
+    document.getElementById("headingtwo").innerHTML = "About something";
+
+    document.getElementById("paragraf1").innerHTML = "";
+
+    document.getElementById("paragraf2").innerHTML = "";
+
+    document.getElementById("paragraf3").innerHTML = "";
+}
