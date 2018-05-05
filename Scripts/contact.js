ce7b93252eb02527b398c9cd700fae09b8a01c2e
CryFar <Hamsik123159@gmail.com>
2018-05-05 02:44:38 +0200

    Some design changes
    Just got a random logo and different colors

--- Scripts/contact.js
+++ Scripts/contact.js
@@ -1,41 +1,21 @@
 function contact() {
-    document.getElementById("heading").innerHTML = "Contact";
-	
-	document.getElementById("underheading").innerHTML = "Something under heading";
-
-    document.getElementById("headingtwo").innerHTML = "Contact on something";
-
-    document.getElementById("paragraf1").innerHTML = "E-mail: something@something.something";
-
-    document.getElementById("paragraf2").innerHTML = "Phone: 1234 123 123";
-
-    document.getElementById("paragraf3").innerHTML = "";
+    document.getElementById("headingtwo").innerHTML = "Contact";
+    document.getElementById("p1").innerHTML = "E-mail: something@something.something";
+    document.getElementById("p2").innerHTML = "Phone: 1234 123 123";
+    document.getElementById("p3").innerHTML = "";
+    document.getElementById("p4").innerHTML = "";
 }
-
 function home() {
-    document.getElementById("heading").innerHTML = "Something";
-	
-	document.getElementById("underheading").innerHTML = "Something under heading";
-
     document.getElementById("headingtwo").innerHTML = "We do something";
-
-    document.getElementById("paragraf1").innerHTML = "We do really something";
-
-    document.getElementById("paragraf2").innerHTML = "Like this";
-
-    document.getElementById("paragraf3").innerHTML = "And that";
+    document.getElementById("p1").innerHTML = "We do really something";
+    document.getElementById("p2").innerHTML = "Like this";
+    document.getElementById("p3").innerHTML = "And that";
+    document.getElementById("p4").innerHTML = "And thisthat";
 }
-
 function about() {
-    document.getElementById("heading").innerHTML = "About";
-	
-	document.getElementById("underheading").innerHTML = "Something under heading";
-
-    document.getElementById("headingtwo").innerHTML = "About something";
-
-    document.getElementById("paragraf1").innerHTML = "";
-
-    document.getElementById("paragraf2").innerHTML = "";
-
-    document.getElementById("paragraf3").innerHTML = "";
+    document.getElementById("headingtwo").innerHTML = "About";
+    document.getElementById("p1").innerHTML = "";
+    document.getElementById("p2").innerHTML = "";
+    document.getElementById("p3").innerHTML = "";
+    document.getElementById("p4").innerHTML = "";
 }
