47c327349a5e1805d3cf21addd225d4acf98abd7
CryFar <Hamsik123159@gmail.com>
2018-05-13 22:25:59 +0200

    Lel

---
+++ Scripts/script.js
@@ -0,0 +1,37 @@
+function contact() {
+    document.getElementById("headingtwo").innerHTML = "Contact";
+    document.getElementById("p1").innerHTML = "E-mail: something@something.something";
+    document.getElementById("p2").innerHTML = "Phone: 1234 123 123";
+    document.getElementById("p3").innerHTML = "";
+    document.getElementById("p4").innerHTML = "";
+}
+function home() {
+    document.getElementById("headingtwo").innerHTML = "Something (Niečo)";
+    document.getElementById("p1").innerHTML = "Toto je len test stránka na ktorej skúšam hocijake dizajny";
+    document.getElementById("p2").innerHTML = "Ako napr.";
+    document.getElementById("p3").innerHTML = "Ten youtube tam dole";
+    document.getElementById("p4").innerHTML = "";
+}
+function about() {
+    document.getElementById("headingtwo").innerHTML = "About";
+    document.getElementById("p1").innerHTML = "";
+    document.getElementById("p2").innerHTML = "";
+    document.getElementById("p3").innerHTML = "";
+    document.getElementById("p4").innerHTML = "";
+}
+
+function video1(){
+    document.getElementById("myframe").src = "https://www.youtube.com/embed/PtbYI8bC3Rk"
+}
+
+function video2(){
+    document.getElementById("myframe").src = "https://www.youtube.com/embed/uoZimlMnXIE"
+}
+
+function video3(){
+    document.getElementById("myframe").src = "https://www.youtube.com/embed/1ahLAzwWE64"
+}
+
+function video4(){
+    document.getElementById("myframe").src = "https://www.youtube.com/embed/A5GOUQ9l66g"
+}
