036b613c65242cb4f5c0c6c01c25a49403484108
CryFar <Hamsik123159@gmail.com>
2018-06-28 23:02:05 +0200

    Build 6
    - added Transition to buttons

--- Scripts/script.js
+++ Scripts/script.js
@@ -2,37 +2,37 @@ function setHTML(elementId, innerHTML) {
   document.getElementById(elementId).innerHTML = innerHTML;
 }
 
-function render(heading, p1, p2, p3) {
-  setHTML("heading", heading);
-  setHTML("p1", p1);
-  setHTML("p2", p2);
-  setHTML("p3", p3);
+function render(h, p1, p2, p3) {
+	setHTML("h", h);
+	setHTML("p1", p1);
+	setHTML("p2", p2);
+	setHTML("p3", p3);
 }
 
 function home() {
-  render(
-    "Something",
-    "Toto je len test stránka na ktorej skúšam hocijake dizajny",
-    "Ako napr.",
-    "Vyber si pesničku:"
+	render(
+		"Something",
+		"Toto je len test stránka na ktorej skúšam hocijake dizajny",
+		"Ako napr.",
+		"Vyber si pesničku:"
   );
 }
 
 function contact() {
-  render(
-    "Contact",
-    "Contact on something",
-    "E-mail: something@something.something",
-    "Phone: 1234 123 123"
+	render(
+		"Contact",
+		"Contact on something",
+		"E-mail: something@something.something",
+		"Phone: 1234 123 123"
   );
 }
 
 function about() {
-  render(
-  "About",
-  "About something",
-  "",
-  ""
+	render(
+		"About",
+		"About something",
+		"",
+		""
   );
 }
 
